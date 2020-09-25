aa0f8fb77ee15d0316a353a3e327d0e0a305c1c4
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2020-09-25 17:03:41 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,36 @@
+class box {
+    constructor(x, y,width,height) {
+      var options = {
+          restitution:0.4,
+          friction:0,
+      }
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.body = Bodies.rectangle(x, y, this.width,this.height, options);
+    this.Visibility = 255;
+    World.add(world, this.body);
+     
+    }
+    display(){
+
+    if(this.body.speed>3){
+      World.remove(world,this.body);
+      push();
+      this.Visibility = this.Visibility-5;
+      pop();
+    }
+    else{
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+    
+      rect(0,0,this.width, this.height);
+      pop();
+    }
+    }
+  }
